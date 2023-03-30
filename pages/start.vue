7de480a500d3db0d6a1f883b48a817eb6da414d8
Patryk <patryk.dawid.wilk@gmail.com>
2023-03-30 23:50:57 +0100

    enter the tower

--- pages/start.vue
+++ pages/start.vue
@@ -8,7 +8,7 @@
         <h1 class="text-center text-white text-9xl flex-1 items-center font-extrabold drop-shadow-xl">Tower of Doom</h1>
       </div>
       <div class="flex-1">
-        <NuxtLink @click="$emit('sound', 'mouseClick')" class="text-white text-center text-2xl font-bold hover:underline hover:animate-pulse" to="/start">Start</NuxtLink>
+        <NuxtLink @click="$emit('sound', 'mouseClick')" class="text-white text-center text-2xl font-bold hover:underline hover:animate-pulse" to="/enter">Enter the Tower</NuxtLink>
       </div>
     </div>
   </div>
