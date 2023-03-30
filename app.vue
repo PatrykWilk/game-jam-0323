b6aaaedae3a57e03d6f531912fa6da51347e9219
Patryk <patryk.dawid.wilk@gmail.com>
2023-03-30 20:12:56 +0100

    init

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <audio ref="audio" src="/audio/song1.mp3" preload="auto" loop></audio>
+  <NuxtPage />
+</template>
+
+<script setup>
+  const audio = ref(null)
+
+  onMounted(() => {
+    audio.value.volume = 0.25
+    // audio.value.play()
+  })
+</script>
+
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="w-screen h-screen max-w-screen max-h-screen flex flex-col align-center justify-center bg-start-bg bg-cover bg-no-repeat bg-center bg-gray-950">
+    <h1 class="text-center text-white text-7xl font-bold mb-10">Tower of Doom</h1>
+    <NuxtLink class="text-white text-center text-xl font-bold" to="/play">Start</NuxtLink>
+  </div>
+</template>
